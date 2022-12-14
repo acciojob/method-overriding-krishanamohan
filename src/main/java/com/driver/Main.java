a57f8bc4e20354923361959ac0c923a1fc837058
KrishanaMohan <ram227209@gmail.com>
2022-12-14 15:20:48 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        public String meth(){
+            return "Invoking method from class B";
+        }
+    }
+  public static void main(String args[]){
+      A a=new A();
+      System.out.println(a.meth());
+      B b=new B();
+      System.out.println(b.meth());
+  }
 }
