8de1fcfb5d02e31f62b84047e560c5b3825691ec
KrishanaMohan <ram227209@gmail.com>
2022-12-15 17:15:41 +0530

    final

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,8 +14,8 @@ public class Main {
         }
     }
   public static void main(String args[]){
-      A a=new A();
-       System.out.println(a.meth());
+    //  A a=new A();
+    //   System.out.println(a.meth());
 
       B b=new B();
       System.out.println(b.meth());
