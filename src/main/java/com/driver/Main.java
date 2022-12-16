12b22960dc41fbec7238742c9d6b3943fd7abcb9
KrishanaMohan <ram227209@gmail.com>
2022-12-16 20:19:11 +0530

    mark

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,8 +14,7 @@ public class Main {
         }
     }
   public static void main(String args[]){
-    //  A a=new A();
-    //   System.out.println(a.meth());
+
 
       B b=new B();
       System.out.println(b.meth());
